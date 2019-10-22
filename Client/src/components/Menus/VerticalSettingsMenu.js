fb46c5100393d4934c28445ca81b19008f42351c
topdev1228 <topdev1228@protonmail.com>
2019-10-22 13:40:50 +0200

    fix menu in settings

--- Client/src/components/Menus/VerticalSettingsMenu.js
+++ Client/src/components/Menus/VerticalSettingsMenu.js
@@ -1,18 +1,85 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {NavLink} from "react-router-dom";
-import {startLogout} from "../../actions/auth";
+import { connect } from 'react-redux';
+import { NavLink } from "react-router-dom";
+import { startLogout } from "../../actions/auth";
 
-const VerticalSettingsMenu = ({ menuItems, logout }) => {
+const VerticalSettingsMenu = ({ menuItems, logout, channels, selectedChannel, selectChannel }) => {
     return (
         <div>
             <aside className="vertical-menu">
-                <MenuItems menuItems={ menuItems } />
-                <SupportSection logout={logout}/>
+                <ProfileInfo selectedChannel={selectedChannel} channels={channels} selectChannel={selectChannel} />
+                <MenuItems menuItems={menuItems} />
+                <SupportSection logout={logout} />
             </aside>
         </div>
     );
 };
+
+class ProfileInfo extends React.Component {
+    state = {
+        channelMenu: false
+    }
+
+    toggleDropdown = () => {
+        this.setState(() => ({
+            channelMenu: !this.state.channelMenu
+        }));
+    }
+
+    render() {
+        const { selectedChannel, channels, selectChannel } = this.props;
+
+        return (
+            <div>
+                <div className="profile-info" onClick={this.toggleDropdown}>
+                    <span className="pull-left profile-img-container">
+                        <img onError={(e) => e.target.src = '/images/dummy_profile.png'} src={selectedChannel.avatar} />
+                        <i className={`fab fa-${selectedChannel.type} ${selectedChannel.type}_bg smallIcon`}></i>
+                    </span>
+                    <div>
+                        <p className="profile-username">{!!selectedChannel.username && `@${selectedChannel.username}`}</p>
+                    </div>
+                </div>
+
+                <ProfileSelectionDropDown
+                    channels={channels}
+                    selectChannel={selectChannel}
+                    isOpen={this.state.channelMenu}
+                />
+            </div>
+
+        );
+    }
+}
+
+const ProfileSelectionDropDown = ({ channels, selectChannel, isOpen }) => (
+    <div className={`channel-selection-menu select-channel ${isOpen ? 'is-open' : ''}`}>
+        <div>
+            {!!channels.length &&
+                channels.map((channel) => (
+                    <ProfileSelectionItem key={channel.id} channel={channel} selectChannel={selectChannel} />
+                ))
+            }
+        </div>
+    </div>
+);
+
+const ProfileSelectionItem = ({ channel, selectChannel }) => (
+    <div className="channel-container">
+        <a href="#" className="block-urls" onClick={(e) => { selectChannel(channel.id) }}>
+            <div className="profile-info ">
+                <span className="profile-img-container">
+                    <img onError={(e) => e.target.src = '/images/dummy_profile.png'} src={channel.avatar} />
+                    <i className={`fab fa-${channel.type} ${channel.type}_bg smallIcon`}></i>
+                </span>
+                <div>
+                    <p className="profile-name" title={channel.name}>{channel.name}</p>
+                    <p className="profile-username">{!!channel.username && `@${channel.username}`}</p>
+                </div>
+            </div>
+        </a>
+    </div>
+);
 
 const MenuItems = ({ menuItems }) => (
     <ul className="v-menu-links clear-both">
@@ -22,7 +89,7 @@ const MenuItems = ({ menuItems }) => (
     </ul>
 );
 
-const SupportSection = ({logout}) => (
+const SupportSection = ({ logout }) => (
     <div className="support">
         <div>
             <a href="mailto:info@uniclixapp.com?Subject=The%20sky%20is%20falling!"><i className="fa fa-comment"></i> SUPPORT</a>
--- Client/src/components/Settings/Settings.js
+++ Client/src/components/Settings/Settings.js
@@ -1,36 +1,69 @@
 import React from 'react';
+import { connect } from "react-redux";
 import VerticalSettingsMenu from "../Menus/VerticalSettingsMenu";
+import channelSelector from "../../selectors/channels";
 import SettingsRouter from '../../routes/SettingsRouter';
 
 const menuItems = [
-    {   
+    {
         id: "profile",
-        displayName: "Profile",
-        uri: "/settings/profile" 
+        displayName: "Personal Info",
+        uri: "/settings/profile"
     },
-    {   
+    {
+        id: "company-info",
+        displayName: "Company Info",
+        uri: "/settings/company-info"
+    },
+    {
         id: "team",
         displayName: "Team",
-        uri: "/settings/team" 
+        uri: "/settings/team"
     },
-    {   
+    {
         id: "billing",
-        displayName: "Billing",
-        uri: "/settings/billing" 
+        displayName: "Manage Accout",
+        uri: "/settings/billing"
     }
 ];
 
-const Settings = () => (
-    <div className="body-wrap">
-        <VerticalSettingsMenu 
-            menuItems={menuItems} 
-            />
-            <div className="body-container">
-                <div className="main-section">
-                    <SettingsRouter/>
+const Settings = ({ channels, selectedChannel, selectChannel }) => {
+    const hasChannel = typeof (selectedChannel.username) !== "undefined";
+    return (
+        <div className="body-wrap">
+            <div>
+                <VerticalSettingsMenu
+                    menuItems={menuItems}
+                    channels={channels}
+                    selectedChannel={selectedChannel}
+                    selectChannel={selectChannel}
+                />
+                <div className="body-container">
+                    <div className="main-section">
+                        <SettingsRouter />
+                    </div>
                 </div>
             </div>
-    </div>
-);
+        </div>
+    );
+}
 
-export default Settings;
+const mapStateToProps = (state) => {
+
+    const unselectedTwitterChannels = { selected: 0, provider: "twitter" };
+    const selectedTwitterChannel = { selected: 1, provider: "twitter" };
+
+    const channels = channelSelector(state.channels.list, unselectedTwitterChannels);
+    const selectedChannel = channelSelector(state.channels.list, selectedTwitterChannel);
+
+    return {
+        channels,
+        selectedChannel: selectedChannel.length ? selectedChannel[0] : {}
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    selectChannel: (id) => dispatch(setTwitterChannel(id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
