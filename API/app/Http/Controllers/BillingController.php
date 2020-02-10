1b288105aa5d436bcf3c8228eefc1e9fa40a7a2c
topdev1228 <topdev1228@protonmail.com>
2020-02-10 20:59:04 +0100

    update billink func

--- API/app/Http/Controllers/BillingController.php
+++ API/app/Http/Controllers/BillingController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Role;
 use App\Models\RoleAddon;
-use Carbon\Carbon;
 use App\Models\Twitter\Channel;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class BillingController extends Controller
 {
@@ -50,7 +50,7 @@ class BillingController extends Controller
         $addon = [
             "addon" => $addon,
             "activeAddon" => $activeAddon,
-            "addonOnGracePeriod" => $addonOnGracePeriod
+            "addonOnGracePeriod" => $addonOnGracePeriod,
         ];
 
         return ["plans" => $plans, "subscription" => $subscription, "addon" => $addon];
@@ -82,14 +82,17 @@ class BillingController extends Controller
             } else {
                 $user->newSubscription($subType, $plan)->create($id);
             }
-            if ($newUsers > 0)
+            if ($newUsers > 0) {
                 $user->subscription('main')->incrementQuantity($newUsers);
+            }
 
             $roleName = $plan;
 
             if ($subType == "main") {
                 $role = Role::where("name", $roleName)->first();
-                if (!$role) return response()->json(["error" => "Plan not found"], 404);
+                if (!$role) {
+                    return response()->json(["error" => "Plan not found"], 404);
+                }
 
                 $user->role_id = $role->id;
                 $user->save();
@@ -99,7 +102,9 @@ class BillingController extends Controller
             } elseif ($subType == "addon") {
 
                 $roleAddon = RoleAddon::where("name", $plan)->first();
-                if (!$roleAddon) return response()->json(["error" => "Addon not found"], 404);
+                if (!$roleAddon) {
+                    return response()->json(["error" => "Addon not found"], 404);
+                }
 
                 $user->roleAddons()->attach($roleAddon->id);
 
@@ -126,6 +131,7 @@ class BillingController extends Controller
             return response()->json(["error" => $th->getMessage()], 500);
         }
     }
+
 
     public function cancelSubscription()
     {
@@ -161,17 +167,24 @@ class BillingController extends Controller
         $plan = $request->input('plan');
         $roleName = explode("_", $plan)[0];
         $role = Role::where("name", $roleName)->first();
-        if (!$role) return response()->json(["error" => "Plan not found"], 404);
+        if (!$role) {
+            return response()->json(["error" => "Plan not found"], 404);
+        }
 
         $user = $this->user;
-        if ($user->channels()->count() > $role->roleLimit->account_limit)
+        if ($user->channels()->count() > $role->roleLimit->account_limit) {
             return response()->json(["error" => "Please delete some social accounts to correspond to the limits of your new plan.", "redirect" => "/accounts"], 403);
+        }
 
-        if ($user->teamMembers()->count() + 1 > $role->roleLimit->team_accounts)
+        if ($user->teamMembers()->count() + 1 > $role->roleLimit->team_accounts) {
             return response()->json(["error" => 'Please delete some team accounts to correspond to the limits of your new plan.', "redirect" => "/settings/team"], 403);
+        }
 
-        if ($plan !== 'free') $user->subscription('main')->swap($plan);
-        else $user->subscription('main')->cancel();
+        if ($plan !== 'free') {
+            $user->subscription('main')->swap($plan);
+        } else {
+            $user->subscription('main')->cancel();
+        }
 
         $user->role_id = $role->id;
         $user->save();
@@ -183,7 +196,9 @@ class BillingController extends Controller
     {
         $addon = $request->input('addon');
         $roleAddon = RoleAddon::where("name", $addon)->first();
-        if (!$roleAddon) return response()->json(["error" => "Addon not found"], 404);;
+        if (!$roleAddon) {
+            return response()->json(["error" => "Addon not found"], 404);
+        }
 
         $user = $this->user;
         $user->roleAddons()->attach($roleAddon->id);
@@ -206,7 +221,9 @@ class BillingController extends Controller
 
             $addon = $request->input('addon');
             $roleAddon = RoleAddon::where("name", $addon)->first();
-            if (!$roleAddon) return response()->json(["error" => "Addon not found"], 404);;
+            if (!$roleAddon) {
+                return response()->json(["error" => "Addon not found"], 404);
+            }
 
             $user = $this->user;
             $user->roleAddons()->detach($roleAddon->id);
--- API/app/Http/Controllers/ChannelController.php
+++ API/app/Http/Controllers/ChannelController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Twitter\Channel;
 
 class ChannelController extends Controller
 {
 
     private $user;
     private $selectedChannel;
-
     /**
      * Create a new controller instance.
      *
@@ -24,17 +24,18 @@ class ChannelController extends Controller
         });
     }
 
-    public function channels() {
+    public function channels()
+    {
         $user = $this->user;
         return $user->allFormattedChannels();
     }
 
     public function select($id)
-    {   
+    {
         $user = $this->user;
         $channel = $user->getChannel($id);
 
-        if($channel){
+        if ($channel) {
             $channel->select($user);
         }
 
@@ -45,19 +46,40 @@ class ChannelController extends Controller
     {
         $channel = $this->user->channels()->find($id);
 
-        if($channel){
+        if ($channel) {
             $channel->delete();
             $channel = $this->user->channels()->first();
 
-            if($channel){
+            if ($channel) {
                 $channel->select();
                 $channel->details->select();
             }
 
+            $this->decreaseSubscription();
+
             return response()->json(["message" => "Channel has been deleted"], 200);
-        }else{
+        } else {
             return response()->json(["error" => "You don't have permission to perform this action"], 403);
         }
     }
 
+    public function decreaseSubscription()
+    {
+        $user = $this->user;
+        $users = Channel::where('user_id', $user->id)->count();
+        $activeSubscription = $this->user->subscribed('main');
+        if ($users == 1) {
+            $user->subscription('main')->cancel();
+            Channel::where('user_id', $user->id)->update(["paid" => false]);
+        }
+        try {
+            if ($activeSubscription) {
+                $user->subscription('main')->updateQuantity($users);
+            }
+
+            return response()->json(["success" => true], 200);
+        } catch (\Throwable $th) {
+            return response()->json(["error" => $th->getMessage()], 500);
+        }
+    }
 }
