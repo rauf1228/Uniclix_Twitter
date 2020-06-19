f6625834e90461fec3a8b75f6f11887617048a54
topdev1228 <topdev1228@protonmail.com>
2020-06-18 22:20:13 -0700

    Fix the Free trial days error

--- Client/src/components/Menus/TopMenu.js
+++ Client/src/components/Menus/TopMenu.js
@@ -7,7 +7,7 @@ import { startLogout } from "../../actions/auth";
 import { withRouter } from "react-router";
 
 const calcTrialDays = (user) => {
-    let msDiff = new Date(user.trial_ends_at).getTime() - new Date().getTime();    //Future date - current date
+    let msDiff = new Date(user.trial_ends_at).getTime() - new Date(user.cur_date).getTime();    //Future date - current date
     var daysToTheEnd = Math.floor(msDiff / (1000 * 60 * 60 * 24));
     return daysToTheEnd >= 0 ? daysToTheEnd + 1 : 0
 }
