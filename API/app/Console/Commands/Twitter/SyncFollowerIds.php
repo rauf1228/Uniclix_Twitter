2dbcbf205ad919f74f610b3ca490a14cd9ec61dd
topdev1228 <topdev1228@protonmail.com>
2020-10-17 14:07:17 -0300

    Added enhancement for follower processes initial query.

--- API/app/Console/Commands/Twitter/SyncFollowerIds.php
+++ API/app/Console/Commands/Twitter/SyncFollowerIds.php
@@ -2,8 +2,11 @@
 
 namespace App\Console\Commands\Twitter;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use App\Models\Twitter\Channel;
+
+use DB;
 
 class SyncFollowerIds extends Command
 {
@@ -38,17 +41,23 @@ class SyncFollowerIds extends Command
      */
     public function handle()
     {
-        $channels = Channel::whereDoesntHave("processes", function($q){
-            $q->where('process_name', 'syncFollowerIds');
-        })->get();
+        $channelIds = \DB::table('twitter_channels')
+            ->join('users', 'twitter_channels.user_id', '=', 'users.id')
+            ->join('channels', 'twitter_channels.channel_id', '=', 'channels.id')
+            ->whereNotExists(function($query)
+            {
+                $query->select(DB::raw(1))
+                    ->from('twitter_processes')
+                    ->whereRaw('twitter_channels.id = twitter_processes.channel_id')
+                    ->where('process_name', 'syncFollowerIds');
+            })
+            ->where('users.trial_ends_at', '>', Carbon::now())
+            ->where('channels.active', true)
+            ->groupBy('twitter_channels.id')
+            ->pluck('twitter_channels.id')
+            ->toArray();
 
         $action = route('sync.follower.ids');
-
-        $channelIds = array();
-
-        foreach ($channels as $channel) {
-            $channelIds[] = $channel->id;
-        }
 
         multiRequest($action, $channelIds);
     }
